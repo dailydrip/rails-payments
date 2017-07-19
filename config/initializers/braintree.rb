dbb1df4a0ed6eac026474e83f98f550cf69b2c32
Franzé Jr <franzejr@gmail.com>
2017-07-19 16:27:32 -0300

    Got devise with extra fields working

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
-Braintree::Configuration.environment = ENV['BT_ENVIRONMENT']
-Braintree::Configuration.merchant_id = ENV['BT_MERCHANT_ID']
-Braintree::Configuration.public_key = ENV['BT_PUBLIC_KEY']
-Braintree::Configuration.private_key = ENV['BT_PRIVATE_KEY']
+#Braintree::Configuration.environment = ENV['BT_ENVIRONMENT']
+#Braintree::Configuration.merchant_id = ENV['BT_MERCHANT_ID']
+#Braintree::Configuration.public_key = ENV['BT_PUBLIC_KEY']
+#Braintree::Configuration.private_key = ENV['BT_PRIVATE_KEY']
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/subscriptions' => 'subscriptions#index'
   post '/subscriptions/subscribe' => 'subscriptions#subscribe'
 
